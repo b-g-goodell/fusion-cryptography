14c0715795384e5f39e87f3e4ed03b543fded898
b-g-goodell <brandon.g.goodell@gmail.com>
2024-01-24 22:53:47 -0700

    gtg

--- api/ntt_api.py
+++ api/ntt_api.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple
-from algebra.ntt import has_primitive_root_of_unity, find_primitive_root, bit_reverse_copy, cooley_tukey_ntt, gentleman_sande_intt, is_odd_prime
+from algebra.ntt import has_prou, find_prou, bit_reverse_copy, cooley_tukey_ntt, gentleman_sande_intt, is_odd_prime
 
 
 def _derived_params(val: List[int], modulus: int, inv_flag: bool) -> Tuple[int, int, int, int, int, List[int]]:
@@ -9,39 +9,39 @@ def _derived_params(val: List[int], modulus: int, inv_flag: bool) -> Tuple[int, 
         raise TypeError(f"Input must be lists of integers and integer.")
     elif not is_odd_prime(val=modulus):
         raise ValueError(f"Modulus must be an odd prime")
-    elif not has_primitive_root_of_unity(modulus=modulus, root_order=2*len(val)):
+    elif not has_prou(mod=modulus, deg=len(val)):
         raise ValueError(f"Modulus does not have primitive root of unity of correct order")
-    degree: int = len(val)
-    root_order: int = 2*degree
+    deg: int = len(val)
+    root_order: int = 2*deg
     halfmod: int = modulus//2
     logmod: int = modulus.bit_length()
-    root: int = find_primitive_root(modulus=modulus, root_order=root_order)
+    root: int = find_prou(mod=modulus, deg=deg)
     if inv_flag:
         root = pow(base=root, exp=modulus - 2, mod=modulus)
-    powers: List[int] = [pow(base=root, exp=i, mod=modulus) for i in range(root_order)]
+    powers: List[int] = [pow(base=root, exp=i, mod=modulus) for i in range(deg)]
     brv_powers: List[int] = bit_reverse_copy(powers)
-    return halfmod, logmod, degree, root_order, root, brv_powers,
+    return halfmod, logmod, deg, root_order, root, brv_powers,
 
 
 def _fwd_ntt_api(val: List[int], modulus: int) -> List[int]:
     """ Compute the forward NTT with cooley_tukey_ntt"""
     halfmod, logmod, degree, root_order, root, brv_root_powers = _derived_params(val=val, modulus=modulus, inv_flag=False)
-    return cooley_tukey_ntt(val=val, modulus=modulus, root_order=root_order, bit_rev_root_powers=brv_root_powers,
-                            halfmod=halfmod, logmod=logmod, root=0)
+    return cooley_tukey_ntt(val=val, mod=modulus, halfmod=halfmod, logmod=logmod, deg=root_order, root=0,
+                            brv_powers=brv_root_powers)
 
 
 def _inv_ntt_api(val: List[int], modulus: int) -> List[int]:
     """ Compute the inverse NTT with gentleman_sande_intt"""
     halfmod, logmod, degree, root_order, inv_root, brv_inv_root_powers = _derived_params(val=val, modulus=modulus, inv_flag=True)
-    return gentleman_sande_intt(val=val, modulus=modulus, root_order=root_order,
-                                bit_rev_inv_root_powers=brv_inv_root_powers, halfmod=halfmod, logmod=logmod, inv_root=0)
+    return gentleman_sande_intt(val=val, mod=modulus, halfmod=halfmod, logmod=logmod, deg=root_order, inv_root=0,
+                                brv_powers=brv_inv_root_powers)
 
 
 def ntt_api(val: List[int], modulus: int, inv_flag: bool) -> List[int]:
     """ Input a list of integers, a modulus, and a flag indicating forward or inverse, output the NTT"""
-    halfmod, logmod, degree, root_order, root, powers = _derived_params(val=val, modulus=modulus, inv_flag=inv_flag)
+    halfmod, logmod, deg, root_order, root_or_inv_root, powers = _derived_params(val=val, modulus=modulus, inv_flag=inv_flag)
     if not inv_flag:
-        return cooley_tukey_ntt(val=val, modulus=modulus, root_order=root_order, bit_rev_root_powers=powers,
-                                halfmod=halfmod, logmod=logmod, root=0)
-    return gentleman_sande_intt(val=val, modulus=modulus, root_order=root_order, bit_rev_inv_root_powers=powers,
-                                halfmod=halfmod, logmod=logmod, inv_root=0)
+        return cooley_tukey_ntt(val=val, mod=modulus, halfmod=halfmod, logmod=logmod, deg=deg, root=root_or_inv_root,
+                                brv_powers=powers)
+    return gentleman_sande_intt(val=val, mod=modulus, halfmod=halfmod, logmod=logmod, deg=deg,
+                                inv_root=root_or_inv_root, brv_powers=powers)
